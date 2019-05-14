2af46dc49cdb9e6ad3037e6691baf9f3662802ed
Zuhairi Rahmad <zuhairi.bit@gmail.com>
2019-05-14 15:48:25 +0900

    update tut May 14th

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,14 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all.order("created_at DESC")
   end
 
   def show
-    @post = Post.find(params[:id])
+  end
+
+  def edit
   end
 
   def new
@@ -14,10 +18,28 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     if @post.save
-      redirect_to posts_path, notice: "You Created the post!"
+      redirect_to @post, notice: "You Created the post!"
     else
       render :new, alert: "You post wasn't created"
     end
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: "Post was successfully update"
+    else
+      render :edit, alert: "your post wasn't updated"
+    end
+  end
+
+  def destroy
+      @post.destroy
+
+      redirect_to posts_path, notice: "Your post was deleted"
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 
   private
